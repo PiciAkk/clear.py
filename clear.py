04cc3f9ee3dfdc797e047277f0f85b93a3dbfa58
Székely Márton <53979153+PiciAkk@users.noreply.github.com>
2021-11-25 19:29:21 +0100

    Update clear.py

--- clear.py
+++ clear.py
@@ -3,8 +3,10 @@ class clear:
         self.clear = "\033[H\033[J"
     def __call__(self):
         print(self.clear, end="")
-        
+   
+clear = clear()
+
 """
-clear()()
-print(clear().clear, end="")
+clear() # calls the `__call__()` function of the `clear` class -> clears the screen
+clear.clear # returns the `clear` variable of the `clear` class
 """
